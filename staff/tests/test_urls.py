6003fa8dc717abcff05542437294b94c8b6124e4
EC2 Default User <ec2-user@ip-172-31-12-42.eu-west-1.compute.internal>
2025-04-05 11:26:11 +0000

    final buildspec pylint and pytest

--- staff/tests/test_urls.py
+++ staff/tests/test_urls.py
@@ -3,11 +3,11 @@ from django.urls import reverse, resolve
 
 def test_staff_dashboard_url():
     url = reverse('staff:staff_dashboard')  # Get the URL from the name
-    assert url == '/staff_dashboard/'  # Check if it matches the expected path
+    assert url == '/staff/staff_dashboard/'  # Check if it matches the expected path
     # 'resolve' checks if the URL points to the right view
     assert resolve(url).func.__name__ == 'staff_dashboard'
 
 def test_item_request_url():
     url = reverse('staff:itemrequest')
-    assert url == '/itemrequest/'  # Adjust this based on your actual URL pattern
+    assert url == '/staff/itemrequest/'  # Adjust this based on your actual URL pattern
     assert resolve(url).func.__name__ == 'itemrequest'
